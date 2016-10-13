1477ae5ac521cd2155fde0b9539e8a2cee427c4b
simnh <simon.hilpert@uni-flensburg.de>
2016-10-13 11:18:40 +0200

    Adapt hesysopt to oemof release branch

--- hesysopt/examples/timesampling-study/run.py
+++ hesysopt/examples/timesampling-study/run.py
@@ -13,11 +13,11 @@ arguments['--scenario_name'] = scenarios[0]
 arguments['--node_data'] = '../simple_example/nodes_flows.csv'
 arguments['--sequence_data'] = '../simple_example/nodes_flows_seq.csv'
 arguments['--start'] = '01/01/2014'
-arguments['--end'] = '12/31/2014'
+arguments['--end'] = '01/02/2014'
 arguments['--freq'] = '1H'
 arguments['--loglevel'] = 'INFO'
 arguments['--solver-output'] = 'True'
-arguments['--solver'] = 'gurobi'
+arguments['--solver'] = 'glpk'
 
 # output directory
 homepath = os.path.expanduser("~")
