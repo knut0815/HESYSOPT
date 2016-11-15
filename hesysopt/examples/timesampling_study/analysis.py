77809b6846ac0a8e36212bfcb070315ecd84aa31
simnh <simon.hilpert@uni-flensburg.de>
2016-11-15 14:27:43 +0100

    Alter analysis

--- hesysopt/examples/timesampling_study/analysis.py
+++ hesysopt/examples/timesampling_study/analysis.py
@@ -16,22 +16,16 @@ el_df = main_df.loc[idx['electrical_balance', 'to_bus', :, :, :]].unstack(['Scen
 el_df.columns = el_df.columns.droplevel([0])
 
 
-scenario_colors = colors = dict(zip(scenarios.keys(),
-                                sns.color_palette("coolwarm_r", len(scenarios.keys()))))
 
+for i in scenarios.keys():
+    pp.summed_production(heat_df[i],
+                         colors=colors['components'])
 
+if False:
+    el_df.columns = el_df.columns.swaplevel(0,1)
+    BP1 = el_df.ix[:, 'BP1']
+    pp.sorted_curves(BP1, colors['scenarios'])
 
-heat_df.columns = heat_df.columns.swaplevel(0,1)
-OB = heat_df.ix[:,'OB']
-
-el_df.columns = el_df.columns.swaplevel(0,1)
-BP1 = el_df.ix[:, 'BP1']
-
-
-pp.sorted_curves(OB, scenario_colors)
-pp.sorted_curves(BP1, scenario_colors)
-
-
-
-
-
+    heat_df.columns = heat_df.columns.swaplevel(0,1)
+    OB = heat_df.ix[:,'OB']
+    pp.sorted_curves(OB, colors['scenarios'])
