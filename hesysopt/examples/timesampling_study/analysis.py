5524e38b2601b966b3161976f7e8b43d4c86e03d
simnh <simon.hilpert@uni-flensburg.de>
2016-11-15 14:08:13 +0100

    Add analysis file

---
+++ hesysopt/examples/timesampling_study/analysis.py
@@ -0,0 +1,37 @@
+
+# coding: utf-8
+
+import seaborn as sns
+import pandas as pd
+from hesysopt.restore_results import restore
+from scenario_builder import scenarios
+import postprocessing as pp
+
+
+main_df, colors = restore(list(scenarios.keys()))
+idx = pd.IndexSlice
+heat_df = main_df.loc[idx['heat_balance','to_bus',:,:,:]].unstack(['Scenario', 'obj_label'])
+heat_df.columns = heat_df.columns.droplevel([0])
+el_df = main_df.loc[idx['electrical_balance', 'to_bus', :, :, :]].unstack(['Scenario', 'obj_label'])
+el_df.columns = el_df.columns.droplevel([0])
+
+
+scenario_colors = colors = dict(zip(scenarios.keys(),
+                                sns.color_palette("coolwarm_r", len(scenarios.keys()))))
+
+
+
+heat_df.columns = heat_df.columns.swaplevel(0,1)
+OB = heat_df.ix[:,'OB']
+
+el_df.columns = el_df.columns.swaplevel(0,1)
+BP1 = el_df.ix[:, 'BP1']
+
+
+pp.sorted_curves(OB, scenario_colors)
+pp.sorted_curves(BP1, scenario_colors)
+
+
+
+
+
